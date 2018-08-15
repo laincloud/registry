267bda00745d82428d6097116cdf0598f2976e46
mac@lowang <longhuiwang@creditease.cn>
2018-08-15 16:18:07 +0800

    add cronjob in registry to cleanup images

---
+++ clean-registry-images.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#########################################################################
+# File Name: cronjob.sh
+# Author: longhui
+# Created Time: 2018-08-15 14:30:36
+#########################################################################
+
+WORK_DIR="/lain/app"
+python2 ${WORK_DIR}/clean-registry-images.py  --delete --num 200
+registry garbage-collect ${WORK_DIR}/config.yaml
